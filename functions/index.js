24c20bff99794d9dd1b2fb3454d5ddfa71093571
dageagle <klyainr@mail.ru>
2025-01-06 22:01:02 +0300

    123

--- functions/index.js
+++ functions/index.js
@@ -145,7 +145,7 @@ vk.updates.on('message_new', async (context) => {
     } 
 
 
-    else if (context.text.toLowerCase() === 'кар') {
+    else if (context.text.toLowerCase() === 'карусель') {
         await context.send({
             message: 'Вот моя карусель:',
             template: JSON.stringify({
