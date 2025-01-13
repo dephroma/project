450b6ef5c6b760aad6221924cb1b36d76be9a4c1
dephroma <klyainr@mail.ru>
2025-01-13 04:49:30 +0300

    31313

--- functions/vkConfig.js
+++ functions/vkConfig.js
@@ -1,5 +1,5 @@
 require('dotenv').config(); // Подключение переменных окружения
-const { VK } = require('vk-io');  //Без киборд???
+const { VK } = require('vk-io');
 
 // Инициализация VK API
 const vk = new VK({
