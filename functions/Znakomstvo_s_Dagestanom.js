e2ad8af35f0f30363733187fde2a37b1e835ac8d
dephroma <klyainr@mail.ru>
2025-01-14 00:00:49 +0300

    31313

--- functions/Znakomstvo_s_Dagestanom.js
+++ functions/Znakomstvo_s_Dagestanom.js
@@ -40,7 +40,7 @@ vk.updates.on('message_new', async (context) => {
     //Оставил пока что только это, тупо скопировал из index, чтобы наверняка не было ошибок. Кроме триггеров, конечно
     if (['биба', 'хуй', 'горох', 'bye'].includes(text)) {
         await context.send({
-            message: "Привет, дорогой 👋👋👋BIBAAAAAAAAAAAAAAAA!👋👋👋\n\nЯ — ваш виртуальный гид. Помогу вам выбрать идеальный тур, отвечу на вопросы и оформлю заявку.\n\nЧем могу помочь?\n\nВыберите опцию в меню ниже. Или напишите ваш вопрос прямо сюда, и я отвечу! 😊",
+            message: "Привет, дорогой 👋👋👋BIBAAAAAAAAAAAAAAAAA!👋👋👋\n\nЯ — ваш виртуальный гид. Помогу вам выбрать идеальный тур, отвечу на вопросы и оформлю заявку.\n\nЧем могу помочь?\n\nВыберите опцию в меню ниже. Или напишите ваш вопрос прямо сюда, и я отвечу! 😊",
             keyboard: Keyboard.keyboard([
                 [Keyboard.textButton({ label: '\u{1f4da} Каталог и бронирование', color: Keyboard.POSITIVE_COLOR })],
                 [Keyboard.textButton({ label: '\u{1f5d3} Даты и цены', color: Keyboard.PRIMARY_COLOR })],
